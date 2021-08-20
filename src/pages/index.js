57f934b368497ed5844005639a8f37fb1b3e129f
Bernie Lawson <lawsonbh@gmail.com>
2021-08-20 17:35:38 -0400

    Added image and related components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Data Doots">
       <p>Documenting my journey one blog post at a time with the occasional Star Wars reference. Oh, and there might be cat photos.</p>
+    <StaticImage
+    alt="Stylish cat held by man with odd mustache"
+    src="../images/profile-pic.png"
+    />
     </Layout>
   )
 }
